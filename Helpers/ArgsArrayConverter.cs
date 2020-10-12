fe22ba6e93a8b6389ea52969311077ae36001327
Jakub Imbrzykowski <jakub.imbrzykowski@nanovotech.pl>
2020-10-12 19:29:43 +0200

    arguments helper added + version bump

---
+++ Helpers/ArgsArrayConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaeveFramework.Helpers
+{
+    public static class ArgsArrayConverter
+    {
+        public static Dictionary<string, string> ArgsToDictionary(this string[] args, string prefix = "-")
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            for (int i = 0; i < args.Length; i++)
+            {
+                try
+                {
+                    if (args[i].StartsWith(prefix))
+                    {
+                        dict.Add(args[i].Remove(0, 1).ToLower(), (args.Length > (i + 1)) ? args[(i + 1)] : string.Empty);
+                    }
+                }
+                catch (Exception) { }
+            }
+            return dict;
+        }
+    }
+}
