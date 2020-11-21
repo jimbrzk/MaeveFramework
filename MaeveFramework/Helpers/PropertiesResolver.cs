e5241f1de4b336fb0c01a8e0ad6a8aa99e5770cb
Jakub Imbrzykowski <jakub.imbrzykowski@gmail.com>
2020-11-21 03:59:44 +0100

    Resolve properties from Dictionary in PropertiesResolver

--- MaeveFramework/Helpers/PropertiesResolver.cs
+++ MaeveFramework/Helpers/PropertiesResolver.cs
@@ -13,15 +13,32 @@ namespace MaeveFramework.Helpers
         /// </summary>
         /// <param name="text"></param>
         /// <param name="properties"></param>
-        /// <param name="addSystemProps">Use default properties</param>
         /// <param name="openTag">Default: "{{"</param>
         /// <param name="closeTag">Default: "}}"</param>
         /// <param name="arraySeparator">Default: ", "</param>
         /// <returns>Text with resolved tags</returns>
-        public static string Resolve(this string text, object properties, bool addSystemProps = true, string openTag = Consts.Helpers.TAG_OPEN, string closeTag = Consts.Helpers.TAG_CLOSE, string arraySeparator = Consts.Helpers.ARRAY_SEPARATOR)
+        public static string Resolve(this string text, object properties, string openTag = Consts.Helpers.TAG_OPEN, string closeTag = Consts.Helpers.TAG_CLOSE, string arraySeparator = Consts.Helpers.ARRAY_SEPARATOR)
         {
-            var dict = GetPropertiesDict(properties, arraySeparator, addSystemProps);
+            var dict = GetPropertiesDict(properties, arraySeparator);
 
+            foreach (var prop in dict)
+            {
+                text = text.Replace(Consts.Helpers.TAG_OPEN + prop.Key + Consts.Helpers.TAG_CLOSE, prop.Value);
+            }
+
+            return text;
+        }
+
+        /// <summary>
+        /// Resolve properties from given Dictionary and populate given text with it's values. Useful for example to populate some message template
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="dict"></param>
+        /// <param name="openTag">Default: "{{"</param>
+        /// <param name="closeTag">Default: "}}"</param>
+        /// <returns></returns>
+        public static string Resolve(this string text, Dictionary<string, string> dict, string openTag = Consts.Helpers.TAG_OPEN, string closeTag = Consts.Helpers.TAG_CLOSE)
+        {
             foreach (var prop in dict)
             {
                 text = text.Replace(Consts.Helpers.TAG_OPEN + prop.Key + Consts.Helpers.TAG_CLOSE, prop.Value);
@@ -35,16 +52,10 @@ namespace MaeveFramework.Helpers
         /// </summary>
         /// <param name="properties"></param>
         /// <param name="arraySeparator"></param>
-        /// <param name="addSystemProps"></param>
         /// <returns></returns>
-        public static Dictionary<string, string> GetPropertiesDict(object properties, string arraySeparator, bool addSystemProps)
+        public static Dictionary<string, string> GetPropertiesDict(object properties, string arraySeparator)
         {
             var propertiesDict = new Dictionary<string, string>();
-            if (addSystemProps)
-            {
-                propertiesDict.Add("$DATETIME", DateTime.Now.ToString(Consts.Helpers.DEFAULT_DATETIME_FORMAT));
-                propertiesDict.Add("$HOSTNAME", Environment.MachineName);
-            }
 
             var props = properties.GetType().GetProperties();
 
@@ -66,7 +77,7 @@ namespace MaeveFramework.Helpers
                 }
                 else if (type.IsClass)
                 {
-                    var classProps = GetPropertiesDict(value, arraySeparator, addSystemProps);
+                    var classProps = GetPropertiesDict(value, arraySeparator);
                     if (classProps.Count > 0)
                        propertiesDict = propertiesDict.Concat(classProps).ToDictionary(k => k.Key, v => v.Value);
                 }
